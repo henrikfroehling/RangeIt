b73833e4ad3d9bf987c05cafb763f8ba61cc2118
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-21 18:51:10 +0100

    Refactor existing implementation

--- Source/Lib/Ranges/View.cs
+++ Source/Lib/Ranges/View.cs
@@ -2,15 +2,7 @@
 {
     public static class View
     {
-        public static int[] Ints(uint count)
-        {
-            var range = new int[count];
-
-            for (int i = 0; i < count; ++i)
-                range[i] = i + 1;
-
-            return range;
-        }
+        public static int[] Ints(uint count) => Ints(1, count);
 
         public static int[] Ints(int startValue, uint count)
         {
