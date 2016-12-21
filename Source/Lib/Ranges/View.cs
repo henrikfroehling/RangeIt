3dea2765c385854a944a1052f21dfc127c0abf1f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-21 19:05:27 +0100

    Add implementation for creating half open integer range

--- Source/Lib/Ranges/View.cs
+++ Source/Lib/Ranges/View.cs
@@ -1,4 +1,6 @@
-﻿namespace Ranges
+﻿using System;
+
+namespace Ranges
 {
     public static class View
     {
@@ -10,6 +12,27 @@
 
             for (int i = 0; i < count; ++i)
                 range[i] = startValue++;
+
+            return range;
+        }
+
+        public static int[] Iota(int from, int to)
+        {
+            if (to < from)
+                throw new ArgumentOutOfRangeException(nameof(to));
+
+            if (from == to)
+                return new int[0];
+
+            var count = to - from;
+            var range = new int[count];
+
+            var i = 0;
+            while (from < to)
+            {
+                range[i] = from++;
+                i++;
+            }
 
             return range;
         }
---
+++ Source/Tests/Ranges.Tests/View_Iota_Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace Ranges.Tests
+{
+    using FluentAssertions;
+    using System;
+    using Xunit;
+
+    public class View_Iota_Tests
+    {
+        [Fact]
+        public void Test_View_Iota()
+        {
+            var range = View.Iota(1, 11);
+
+            range.Should().NotBeNull()
+                          .And.NotBeEmpty()
+                          .And.HaveCount(10)
+                          .And.ContainInOrder(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        }
+
+        [Fact]
+        public void Test_View_Iota_With_To_SmallerThan_From()
+        {
+            Action act = () => View.Iota(1, 0);
+            act.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Test_View_Iota_WithEqualArguments()
+        {
+            var range = View.Iota(1, 1);
+
+            range.Should().NotBeNull()
+                          .And.BeEmpty()
+                          .And.HaveCount(0);
+        }
+    }
+}
