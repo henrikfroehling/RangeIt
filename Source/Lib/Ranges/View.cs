440e1a4bc9c7f1b7010d9d2ea50e8438de783e0d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-21 18:50:09 +0100

    Add implementation for creating integer range with custom start value

--- Source/Lib/Ranges/View.cs
+++ Source/Lib/Ranges/View.cs
@@ -11,5 +11,15 @@
 
             return range;
         }
+
+        public static int[] Ints(int startValue, uint count)
+        {
+            var range = new int[count];
+
+            for (int i = 0; i < count; ++i)
+                range[i] = startValue++;
+
+            return range;
+        }
     }
 }
--- Source/Tests/Ranges.Tests/View_Ints_Tests.cs
+++ Source/Tests/Ranges.Tests/View_Ints_Tests.cs
@@ -25,5 +25,16 @@
                           .And.BeEmpty()
                           .And.HaveCount(0);
         }
+
+        [Fact]
+        public void Test_View_Ints_WithStartValue()
+        {
+            var range = View.Ints(5, 10);
+
+            range.Should().NotBeNull()
+                          .And.NotBeEmpty()
+                          .And.HaveCount(10)
+                          .And.ContainInOrder(5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+        }
     }
 }
