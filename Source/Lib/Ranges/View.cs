2dfc695c576256e602fd6bb4cf5caa42037a9b3b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-21 18:42:37 +0100

    Add implementation for creating integer range

---
+++ Source/Lib/Ranges/View.cs
@@ -0,0 +1,15 @@
+﻿namespace Ranges
+{
+    public static class View
+    {
+        public static int[] Ints(uint count)
+        {
+            var range = new int[count];
+
+            for (int i = 0; i < count; ++i)
+                range[i] = i + 1;
+
+            return range;
+        }
+    }
+}
---
+++ Source/Tests/Ranges.Tests/View_Ints_Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace Ranges.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class View_Ints_Tests
+    {
+        [Fact]
+        public void Test_View_Ints()
+        {
+            var range = View.Ints(10);
+
+            range.Should().NotBeNull()
+                          .And.NotBeEmpty()
+                          .And.HaveCount(10)
+                          .And.ContainInOrder(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        }
+
+        [Fact]
+        public void Test_View_Ints_WithZeroCount()
+        {
+            var range = View.Ints(0);
+
+            range.Should().NotBeNull()
+                          .And.BeEmpty()
+                          .And.HaveCount(0);
+        }
+    }
+}
