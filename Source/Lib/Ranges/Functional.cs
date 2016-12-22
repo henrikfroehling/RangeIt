a0dfd90bc3f2f0684ef880bb6a35b8380859f78a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-22 15:32:09 +0100

    Add functional lower case wrapper methods

---
+++ Source/Lib/Ranges/Functional.cs
@@ -0,0 +1,24 @@
+﻿namespace Ranges
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Functional
+    {
+        public static IEnumerable<int> ints(uint count) => Range.Ints(count);
+
+        public static IEnumerable<int> ints(int startValue, uint count) => Range.Ints(startValue, count);
+
+        public static IEnumerable<int> iota(int from, int to) => Range.Iota(from, to);
+
+        public static IEnumerable<T> iota<T>(T startValue, uint count, Func<T, T> generator) => Range.Iota(startValue, count, generator);
+
+        public static IEnumerable<T> iota<T>(T startValue, uint count, Func<T> generator) => Range.Iota(startValue, count, generator);
+
+        public static IEnumerable<T> iota<T>(uint count, Func<T> generator) => Range.Iota(count, generator);
+
+        public static IEnumerable<T> iota<T>(Func<T, T> generator, Func<T, bool> cancellationPredicate) => Range.Iota(generator, cancellationPredicate);
+
+        public static IEnumerable<T> iota<T>(T startValue, Func<T, T> generator, Func<T, bool> cancellationPredicate) => Range.Iota(startValue, generator, cancellationPredicate);
+    }
+}
