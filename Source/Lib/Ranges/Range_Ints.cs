479e98368b629a0ad136dff1ac90578a0ceecd1f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-22 17:30:42 +0100

    Make Range class partial

---
+++ Source/Lib/Ranges/Range_Ints.cs
@@ -0,0 +1,15 @@
+﻿namespace Ranges
+{
+    using System.Collections.Generic;
+
+    public static partial class Range
+    {
+        public static IEnumerable<int> Ints(uint count) => Ints(1, count);
+
+        public static IEnumerable<int> Ints(int startValue, uint count)
+        {
+            for (int i = 0; i < count; ++i)
+                yield return startValue++;
+        }
+    }
+}
--- Source/Lib/Ranges/Range_Iota.cs
+++ Source/Lib/Ranges/Range_Iota.cs
@@ -3,16 +3,8 @@
     using System;
     using System.Collections.Generic;
 
-    public static class Range
+    public static partial class Range
     {
-        public static IEnumerable<int> Ints(uint count) => Ints(1, count);
-
-        public static IEnumerable<int> Ints(int startValue, uint count)
-        {
-            for (int i = 0; i < count; ++i)
-                yield return startValue++;
-        }
-
         public static IEnumerable<int> Iota(int from, int to)
         {
             while (from < to)
