261d072806c2abfccd5759c4821f34df327ca1f1
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 21:16:01 +0100

    Add Current property in IIterator<T>

--- Source/Lib/RangeIt/Iterators/Interfaces/IIterator'1.cs
+++ Source/Lib/RangeIt/Iterators/Interfaces/IIterator'1.cs
@@ -4,6 +4,6 @@
 
     public interface IIterator<T> : IIterable, IEnumerable<T>
     {
-
+        T Current { get; set; }
     }
 }
--- Source/Tests/RangeIt.Tests/Iterators/Interfaces/IIterator'1_Tests.cs
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/IIterator'1_Tests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using RangeIt.Iterators.Interfaces;
     using System.Collections.Generic;
+    using System.Linq;
     using Xunit;
 
     [Collection("IIterator<T>.Tests")]
@@ -24,6 +25,18 @@
         public void Test_IIterator_1_DerivesFrom_IEnumerable_1_Interface()
         {
             typeof(IIterator<int>).GetInterfaces().Should().Contain(typeof(IEnumerable<int>));
+        }
+
+        [Fact]
+        public void Test_IIterator_1_Has_Current_Property()
+        {
+            var uriTemplatePropertyInfo = typeof(IIterator<int>).GetProperties()
+                                                                .Where(p => p.Name == "Current")
+                                                                .FirstOrDefault();
+
+            uriTemplatePropertyInfo.CanRead.Should().BeTrue();
+            uriTemplatePropertyInfo.CanWrite.Should().BeTrue();
+            uriTemplatePropertyInfo.PropertyType.Should().Be(typeof(int));
         }
     }
 }
