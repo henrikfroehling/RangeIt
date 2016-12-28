9b8ab5c5a4007cd72e99aa36c8fbaa6b61990755
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 21:10:52 +0100

    Add generic interface with one type parameter for iterators

---
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/IIterator'1_Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace RangeIt.Tests.Iterators.Interfaces
+{
+    using FluentAssertions;
+    using RangeIt.Iterators.Interfaces;
+    using Xunit;
+
+    [Collection("IIterator<T>.Tests")]
+    public class IIterator_1_Tests
+    {
+        [Fact]
+        public void Test_IIterator_1_Is_Interface()
+        {
+            typeof(IIterator<>).IsInterface.Should().BeTrue();
+        }
+    }
+}
