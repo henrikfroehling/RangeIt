faaa06f2030e8b36d364ffb0a72db1549ed5cee9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 21:28:44 +0100

    Add interface for non generic const iterators

---
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/IConstIterator_Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace RangeIt.Tests.Iterators.Interfaces
+{
+    using FluentAssertions;
+    using RangeIt.Iterators.Interfaces;
+    using Xunit;
+
+    [Collection("IConstIterator.Tests")]
+    public class IConstIterator_Tests
+    {
+        [Fact]
+        public void Test_IConstIterator_Is_Interface()
+        {
+            typeof(IConstIterator).IsInterface.Should().BeTrue();
+        }
+    }
+}
