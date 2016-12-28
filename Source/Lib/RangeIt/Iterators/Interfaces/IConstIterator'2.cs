9be9662bb212aef2a13d38a8b69a4f73710b0862
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 21:40:27 +0100

    Add generic interface with two type parameters for const iterators

---
+++ Source/Lib/RangeIt/Iterators/Interfaces/IConstIterator'2.cs
@@ -0,0 +1,7 @@
+﻿namespace RangeIt.Iterators.Interfaces
+{
+    public interface IConstIterator<T, U>
+    {
+
+    }
+}
---
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/IConstIterator'2_Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace RangeIt.Tests.Iterators.Interfaces
+{
+    using FluentAssertions;
+    using RangeIt.Iterators.Interfaces;
+    using Xunit;
+
+    [Collection("IConstIterator<T, U>.Tests")]
+    public class IConstIterator_2_Tests
+    {
+        [Fact]
+        public void Test_IConstIterator_2_Is_Interface()
+        {
+            typeof(IConstIterator<,>).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_IConstIterator_2_Is_Generic()
+        {
+            typeof(IConstIterator<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(IConstIterator<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+    }
+}
