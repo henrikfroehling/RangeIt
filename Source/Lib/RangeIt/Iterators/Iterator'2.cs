547567c8ad950a1af167e67e555275087250fbb7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 22:33:53 +0100

    Add constructors in Iterator<T, U>

--- Source/Lib/RangeIt/Iterators/Iterator'2.cs
+++ Source/Lib/RangeIt/Iterators/Iterator'2.cs
@@ -3,10 +3,34 @@
     using Interfaces;
     using System;
     using System.Collections;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
 
     public sealed class Iterator<T, U> : IIterator<T, U>
     {
+        private Iterator() { }
+
+        public Iterator(KeyValuePair<T, U>[] items)
+        {
+
+        }
+
+        public Iterator(Dictionary<T, U> dictionary)
+        {
+
+        }
+
+        public Iterator(ReadOnlyDictionary<T, U> dictionary)
+        {
+
+        }
+
+        public Iterator(ConcurrentDictionary<T, U> dictionary)
+        {
+
+        }
+
         public KeyValuePair<T, U> Current
         {
             get
