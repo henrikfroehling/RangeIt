a6613163239936afb864139df4088ef5277fc38a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 21:00:02 +0100

    Add non generic interface for iterators

---
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/IIterator_Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace RangeIt.Tests.Iterators.Interfaces
+{
+    using FluentAssertions;
+    using RangeIt.Iterators.Interfaces;
+    using Xunit;
+
+    [Collection("IIterator.Tests")]
+    public class IIterator_Tests
+    {
+        [Fact]
+        public void Test_IIterator_IsInterface()
+        {
+            typeof(IIterator).IsInterface.Should().BeTrue();
+        }
+    }
+}
