dfdbe983698532fbcc8252584bbb5a418fb0045f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 22:23:19 +0100

    Add class for generic iterator implementation with one type parameter

---
+++ Source/Tests/RangeIt.Tests/Iterators/Iterator'1_Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace RangeIt.Tests.Iterators
+{
+    using FluentAssertions;
+    using RangeIt.Iterators;
+    using Xunit;
+
+    [Collection("Iterator<T>.Tests")]
+    public class Iterator_1_Tests
+    {
+        [Fact]
+        public void Test_Iterator_1_IsNot_Abstract()
+        {
+            typeof(Iterator<>).IsAbstract.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test_Iterator_1_Is_Sealed()
+        {
+            typeof(Iterator<>).IsSealed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_Iterator_1_Is_Generic()
+        {
+            typeof(Iterator<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(Iterator<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+    }
+}
