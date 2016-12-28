fe062dd82f15e21422bf49b144a476347a16602b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 22:52:43 +0100

    Add class for generic const iterator implementation with two type parameters

---
+++ Source/Tests/RangeIt.Tests/Iterators/ConstIterator'2_Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace RangeIt.Tests.Iterators
+{
+    using FluentAssertions;
+    using RangeIt.Iterators;
+    using Xunit;
+
+    [Collection("ConstIterator<T, U>.Tests")]
+    public class ConstIterator_2_Tests
+    {
+        [Fact]
+        public void Test_ConstIterator_2_IsNot_Abstract()
+        {
+            typeof(ConstIterator<,>).IsAbstract.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test_ConstIterator_2_Is_Sealed()
+        {
+            typeof(ConstIterator<,>).IsSealed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ConstIterator_2_Is_Generic()
+        {
+            typeof(ConstIterator<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ConstIterator<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+    }
+}
