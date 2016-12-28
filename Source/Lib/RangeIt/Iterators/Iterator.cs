7f0b202ca0a66f4b988bc3c8cc49af2af7b5bdfc
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 21:52:45 +0100

    Implement IIterator interface in Iterator

--- Source/Lib/RangeIt/Iterators/Iterator.cs
+++ Source/Lib/RangeIt/Iterators/Iterator.cs
@@ -1,7 +1,37 @@
 ﻿namespace RangeIt.Iterators
 {
-    public sealed class Iterator
-    {
+    using Interfaces;
+    using System;
+    using System.Collections;
 
+    public sealed class Iterator : IIterator
+    {
+        public object Current
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Previous()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Next()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Source/Tests/RangeIt.Tests/Iterators/Iterator_Tests.cs
+++ Source/Tests/RangeIt.Tests/Iterators/Iterator_Tests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using RangeIt.Iterators;
+    using RangeIt.Iterators.Interfaces;
     using Xunit;
 
     [Collection("Iterator.Tests")]
@@ -17,6 +18,12 @@
         public void Test_Iterator_Is_Sealed()
         {
             typeof(Iterator).IsSealed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_Iterator_DerivesFrom_IIterator_Interface()
+        {
+            typeof(Iterator).GetInterfaces().Should().Contain(typeof(IIterator));
         }
     }
 }
