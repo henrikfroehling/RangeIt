0e6a82c3d5f34324c9a7df34fff1e55181ec949e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 22:37:55 +0100

    Add class for non generic const iterator implementation

---
+++ Source/Tests/RangeIt.Tests/Iterators/ConstIterator_Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace RangeIt.Tests.Iterators
+{
+    using FluentAssertions;
+    using RangeIt.Iterators;
+    using Xunit;
+
+    [Collection("ConstIterator.Tests")]
+    public class ConstIterator_Tests
+    {
+        [Fact]
+        public void Test_ConstIterator_IsNot_Abstract()
+        {
+            typeof(ConstIterator).IsAbstract.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test_ConstIterator_Is_Sealed()
+        {
+            typeof(ConstIterator).IsSealed.Should().BeTrue();
+        }
+    }
+}
