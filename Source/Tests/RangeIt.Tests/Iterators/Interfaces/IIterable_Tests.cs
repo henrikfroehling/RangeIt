f2da830f1e35f7f3bde274406553f24f61918f1b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-28 20:51:25 +0100

    Add basic interface for iterators

---
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/IIterable_Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace RangeIt.Tests.Iterators.Interfaces
+{
+    using FluentAssertions;
+    using RangeIt.Iterators.Interfaces;
+    using Xunit;
+
+    [Collection("IIterable.Tests")]
+    public class IIterable_Tests
+    {
+        [Fact]
+        public void Test_IIterable_IsInterface()
+        {
+            typeof(IIterable).IsInterface.Should().BeTrue();
+        }
+    }
+}
