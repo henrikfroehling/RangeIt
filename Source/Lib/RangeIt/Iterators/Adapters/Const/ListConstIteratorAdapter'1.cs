149a8cf487b5983557a3e6609ee2cafb817831a1
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 23:22:53 +0100

    Add list iterator adapter implementation

---
+++ Source/Lib/RangeIt/Iterators/Adapters/Const/ListConstIteratorAdapter'1.cs
@@ -0,0 +1,15 @@
+﻿namespace RangeIt.Iterators.Adapters.Const
+{
+    using Interfaces.Adapters;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    internal sealed class ListConstIteratorAdapter<T> : BaseListIteratorAdapter<T>, IConstIteratorAdapter<T>
+    {
+        internal ListConstIteratorAdapter(List<T> list, bool isEnd = false) : base(list, isEnd) { }
+
+        public T Current => _current;
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Source/Lib/RangeIt/Iterators/Adapters/NotConst/ListIteratorAdapter'1.cs
@@ -0,0 +1,30 @@
+﻿namespace RangeIt.Iterators.Adapters.NotConst
+{
+    using Interfaces.Adapters;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    internal sealed class ListIteratorAdapter<T> : BaseListIteratorAdapter<T>, IIteratorAdapter<T>
+    {
+        internal ListIteratorAdapter(List<T> list, bool isEnd = false) : base(list, isEnd) { }
+
+        public T Current
+        {
+            get { return _current; }
+
+            set
+            {
+                if (_isEnd)
+                    return;
+
+                if (_index >= 0 && _index < _list.Count)
+                {
+                    _current = value;
+                    _list[_index] = _current;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
