fb09b18ce017f71c7bffd5a04c4ef8a072b527cd
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 20:01:09 +0100

    Add implementation for DictionaryConstIterator<T, U>

---
+++ Source/Tests/RangeIt.Tests/Iterators/Detail/Const/DictionaryConstIterator'2_Tests.cs
@@ -0,0 +1,429 @@
+﻿namespace RangeIt.Tests.Iterators.Detail.Const
+{
+    using FluentAssertions;
+    using RangeIt.Iterators;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
+    [Collection("DictionaryConstIterator<T, U>.Tests")]
+    public class DictionaryConstIterator_2_Tests
+    {
+        [Fact]
+        public void Test_DictionaryConstIterator_2_Begin_Ctor_WithEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>();
+            var it = dictionary.ConstBegin();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_End_Ctor_WithEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>();
+            var it = dictionary.ConstEnd();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_Begin_Iteration_WithEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>();
+            var it = dictionary.ConstBegin();
+
+            it.Next().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+
+            it.Previous().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_End_Iteration_WithEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>();
+            var it = dictionary.ConstEnd();
+
+            it.Next().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+
+            it.Previous().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_Begin_Ctor_WithNotEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                ["one"] = 1,
+                ["two"] = 2,
+                ["three"] = 3,
+                ["four"] = 4,
+                ["five"] = 5
+            };
+
+            var it = dictionary.ConstBegin();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_End_Ctor_WithNotEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                ["one"] = 1,
+                ["two"] = 2,
+                ["three"] = 3,
+                ["four"] = 4,
+                ["five"] = 5
+            };
+
+            var it = dictionary.ConstEnd();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_Begin_Iteration_WithNotEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                ["one"] = 1,
+                ["two"] = 2,
+                ["three"] = 3,
+                ["four"] = 4,
+                ["five"] = 5
+            };
+
+            var it = dictionary.ConstBegin();
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(0);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("one");
+            it.Value.Should().Be(1);
+
+            // 1. back iteration
+            it.Previous().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(0);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("one");
+            it.Value.Should().Be(1);
+
+            // 2. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("two");
+            it.Value.Should().Be(2);
+
+            // 3. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(2);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("three");
+            it.Value.Should().Be(3);
+
+            // 4. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(3);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("four");
+            it.Value.Should().Be(4);
+
+            // 5. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(4);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("five");
+            it.Value.Should().Be(5);
+
+            // 6. iteration
+            it.Next().Should().BeFalse();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(dictionary.Count());
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+
+            // 1. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(4);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("five");
+            it.Value.Should().Be(5);
+
+            // 2. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(3);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("four");
+            it.Value.Should().Be(4);
+
+            // 3. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(2);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("three");
+            it.Value.Should().Be(3);
+
+            // 4. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("two");
+            it.Value.Should().Be(2);
+
+            // 5. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(0);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("one");
+            it.Value.Should().Be(1);
+
+            // 6. back iteration
+            it.Previous().Should().BeFalse();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_End_Iteration_WithNotEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                ["one"] = 1,
+                ["two"] = 2,
+                ["three"] = 3,
+                ["four"] = 4,
+                ["five"] = 5
+            };
+
+            var it = dictionary.ConstEnd();
+
+            // 1. iteration
+            it.Next().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+
+            // 1. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(4);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("five");
+            it.Value.Should().Be(5);
+
+            // 2. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(3);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("four");
+            it.Value.Should().Be(4);
+
+            // 3. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(2);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("three");
+            it.Value.Should().Be(3);
+
+            // 4. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("two");
+            it.Value.Should().Be(2);
+
+            // 5. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(0);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("one");
+            it.Value.Should().Be(1);
+
+            // 6. back iteration
+            it.Previous().Should().BeFalse();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(0);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("one");
+            it.Value.Should().Be(1);
+
+            // 2. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(1);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("two");
+            it.Value.Should().Be(2);
+
+            // 3. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(2);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("three");
+            it.Value.Should().Be(3);
+
+            // 4. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(3);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("four");
+            it.Value.Should().Be(4);
+
+            // 5. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(4);
+            it.Current.Should().NotBeNull();
+            it.Key.Should().NotBeNull().And.Be("five");
+            it.Value.Should().Be(5);
+
+            // 6. iteration
+            it.Next().Should().BeFalse();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(dictionary.Count());
+            it.Current.Should().NotBeNull();
+            it.Key.Should().BeNull();
+            it.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_Begin_IsValid()
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                ["one"] = 1,
+                ["two"] = 2,
+                ["three"] = 3,
+                ["four"] = 4,
+                ["five"] = 5
+            };
+
+            var it = dictionary.ConstBegin();
+
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.IsValid.Should().BeFalse();
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(0);
+            it.IsValid.Should().BeTrue();
+
+            // 2. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(1);
+            it.IsValid.Should().BeTrue();
+
+            // 3. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(2);
+            it.IsValid.Should().BeTrue();
+
+            // 4. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(3);
+            it.IsValid.Should().BeTrue();
+
+            // 5. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(4);
+            it.IsValid.Should().BeTrue();
+
+            // 6. iteration
+            it.Next().Should().BeFalse();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(5);
+            it.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test_DictionaryConstIterator_2_End_IsValid()
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                ["one"] = 1,
+                ["two"] = 2,
+                ["three"] = 3,
+                ["four"] = 4,
+                ["five"] = 5
+            };
+
+            var it = dictionary.ConstEnd();
+
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(-1);
+            it.IsValid.Should().BeFalse();
+        }
+    }
+}
