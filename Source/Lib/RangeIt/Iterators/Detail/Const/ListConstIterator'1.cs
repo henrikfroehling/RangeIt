f4948c1a35c4a1c2b1e88eedd27f5fbda1088d51
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 18:54:11 +0100

    Add implementation for ListConstIterator<T>

--- Source/Lib/RangeIt/Iterators/Detail/Const/ListConstIterator'1.cs
+++ Source/Lib/RangeIt/Iterators/Detail/Const/ListConstIterator'1.cs
@@ -7,61 +7,87 @@
 
     internal struct ListConstIterator<T> : IConstIterator<T>
     {
+        private List<T> _list;
+        private T _current;
+        private int _index;
+        private bool _isEnd;
+
         internal ListConstIterator(List<T> list, bool isEnd = false)
         {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
 
+            _list = list;
+            _current = default(T);
+            _isEnd = isEnd;
+            _index = -1;
         }
 
-        public T Current
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public T Current => _current;
 
-        public int Index
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public int Index => _index;
 
-        public bool IsEndIterator
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public bool IsEndIterator => _isEnd;
 
-        public bool IsValid
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public bool IsValid => !IsEndIterator && Index >= 0 && Index < _list.Count;
 
         public bool Previous()
         {
-            throw new NotImplementedException();
+            var count = _list.Count;
+
+            if (count == 0)
+                return false;
+
+            if (_index == 0)
+            {
+                _isEnd = false;
+                _index = -1;
+                _current = default(T);
+                return false;
+            }
+
+            if (_isEnd)
+                _index = count;
+
+            if (_index >= 1)
+            {
+                _isEnd = false;
+                --_index;
+                _current = _list[_index];
+                return true;
+            }
+
+            return false;
         }
 
         public bool Next()
         {
-            throw new NotImplementedException();
+            var count = _list.Count;
+
+            if (_isEnd || count == 0)
+                return false;
+
+            if (_index == count - 1)
+            {
+                _index = count;
+                _isEnd = true;
+                _current = default(T);
+                return false;
+            }
+
+            if (_index < count - 1)
+            {
+                _index++;
+                _current = _list[_index];
+                return true;
+            }
+
+            _isEnd = true;
+            return false;
         }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerator<T> GetEnumerator() => _list.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
