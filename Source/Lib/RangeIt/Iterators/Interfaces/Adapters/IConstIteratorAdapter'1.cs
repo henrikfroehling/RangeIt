263be9453fff783d4c8585d7244af3f829d1210d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 23:04:30 +0100

    Add generic IConstIteratorAdapter interface with one type parameter

---
+++ Source/Lib/RangeIt/Iterators/Interfaces/Adapters/IConstIteratorAdapter'1.cs
@@ -0,0 +1,9 @@
+﻿namespace RangeIt.Iterators.Interfaces.Adapters
+{
+    using System.Collections.Generic;
+
+    internal interface IConstIteratorAdapter<T> : IConstIterator<T>, IIterable, IEnumerable<T>
+    {
+
+    }
+}
---
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/Adapters/IConstIteratorAdapter'1_Tests.cs
@@ -0,0 +1,43 @@
+﻿namespace RangeIt.Tests.Iterators.Interfaces.Adapters
+{
+    using FluentAssertions;
+    using RangeIt.Iterators.Interfaces;
+    using RangeIt.Iterators.Interfaces.Adapters;
+    using System.Collections.Generic;
+    using Xunit;
+
+    [Collection("IConstIteratorAdapter<T>.Tests")]
+    public class IConstIteratorAdapter_1_Tests
+    {
+        [Fact]
+        public void Test_IConstIteratorAdapter_1_Is_Interface()
+        {
+            typeof(IConstIteratorAdapter<>).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_IConstIteratorAdapter_1_Is_Generic()
+        {
+            typeof(IConstIteratorAdapter<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(IConstIteratorAdapter<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [Fact]
+        public void Test_IConstIteratorAdapter_1_DerivesFrom_IConstIterator_Interface()
+        {
+            typeof(IConstIteratorAdapter<int>).GetInterfaces().Should().Contain(typeof(IConstIterator<int>));
+        }
+
+        [Fact]
+        public void Test_IConstIteratorAdapter_1_DerivesFrom_IIterable_Interface()
+        {
+            typeof(IConstIteratorAdapter<>).GetInterfaces().Should().Contain(typeof(IIterable));
+        }
+
+        [Fact]
+        public void Test_IConstIteratorAdapter_1_DerivesFrom_IEnumerable_Interface()
+        {
+            typeof(IConstIteratorAdapter<int>).GetInterfaces().Should().Contain(typeof(IEnumerable<int>));
+        }
+    }
+}
