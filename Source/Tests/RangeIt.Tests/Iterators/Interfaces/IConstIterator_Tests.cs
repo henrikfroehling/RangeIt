5156fc0bdaa2328cb6d3d5fd1b22dd97fd9b17f4
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 23:33:36 +0100

    Remove base interfaces from IConstIterator interface

--- Source/Tests/RangeIt.Tests/Iterators/Interfaces/IConstIterator_Tests.cs
+++ Source/Tests/RangeIt.Tests/Iterators/Interfaces/IConstIterator_Tests.cs
@@ -2,7 +2,6 @@
 {
     using FluentAssertions;
     using RangeIt.Iterators.Interfaces;
-    using System.Collections;
     using System.Linq;
     using Xunit;
 
@@ -13,18 +12,6 @@
         public void Test_IConstIterator_Is_Interface()
         {
             typeof(IConstIterator).IsInterface.Should().BeTrue();
-        }
-
-        [Fact]
-        public void Test_IConstIterator_DerivesFrom_IIterable_Interface()
-        {
-            typeof(IConstIterator).GetInterfaces().Should().Contain(typeof(IIterable));
-        }
-
-        [Fact]
-        public void Test_IConstIterator_DerivesFrom_IEnumerable_Interface()
-        {
-            typeof(IConstIterator).GetInterfaces().Should().Contain(typeof(IEnumerable));
         }
 
         [Fact]
