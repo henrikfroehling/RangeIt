e344918597019ef12acd01922c95f4014c7b863e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 23:00:24 +0100

    Add IConstIteratorAdapter interface

---
+++ Source/Lib/RangeIt/Iterators/Interfaces/Adapters/IConstIteratorAdapter.cs
@@ -0,0 +1,9 @@
+﻿namespace RangeIt.Iterators.Interfaces.Adapters
+{
+    using System.Collections;
+
+    internal interface IConstIteratorAdapter : IConstIterator, IIterable, IEnumerable
+    {
+
+    }
+}
