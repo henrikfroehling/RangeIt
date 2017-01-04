3f0d5a475d58c4144e03ca863ff811d4f00b1bcd
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 23:52:39 +0100

    Implement interfaces in generic ConstIterator struct with two type parameters

--- Source/Lib/RangeIt/Iterators/ConstIterator'2.cs
+++ Source/Lib/RangeIt/Iterators/ConstIterator'2.cs
@@ -1,10 +1,13 @@
 ﻿namespace RangeIt.Iterators
 {
+    using Interfaces;
+    using System;
+    using System.Collections;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
 
-    public struct ConstIterator<T, U>
+    public struct ConstIterator<T, U> : IConstIterator<T, U>, IIterable, IEnumerable<KeyValuePair<T, U>>
     {
         public ConstIterator(KeyValuePair<T, U>[] items)
         {
@@ -44,6 +47,74 @@
         internal ConstIterator(ConcurrentDictionary<T, U> dictionary, bool isEnd)
         {
 
+        }
+
+        public KeyValuePair<T, U> Current
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public T Key
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public U Value
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Index
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsEndIterator
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Previous()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Next()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<T, U>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Source/Tests/RangeIt.Tests/Iterators/ConstIterator'2_Tests.cs
+++ Source/Tests/RangeIt.Tests/Iterators/ConstIterator'2_Tests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using RangeIt.Iterators;
     using RangeIt.Iterators.Interfaces;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using Xunit;
@@ -33,6 +34,18 @@
         public void Test_ConstIterator_2_Implements_IConstIterator_2_Interface()
         {
             typeof(ConstIterator<int, float>).GetInterfaces().Should().Contain(typeof(IConstIterator<int, float>));
+        }
+
+        [Fact]
+        public void Test_ConstIterator_2_Implements_IIterable_Interface()
+        {
+            typeof(ConstIterator<int, float>).GetInterfaces().Should().Contain(typeof(IIterable));
+        }
+
+        [Fact]
+        public void Test_ConstIterator_2_Implements_IEnumerable_Interface()
+        {
+            typeof(ConstIterator<int, float>).GetInterfaces().Should().Contain(typeof(IEnumerable<KeyValuePair<int, float>>));
         }
 
         [Fact]
