194abd8baa8103fdef6771ef1d4fe8ea70ea3dee
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 23:09:37 +0100

    Add generic IConstIteratorAdapter interface with two type parameters

---
+++ Source/Lib/RangeIt/Iterators/Interfaces/Adapters/IConstIteratorAdapter'2.cs
@@ -0,0 +1,9 @@
+﻿namespace RangeIt.Iterators.Interfaces.Adapters
+{
+    using System.Collections.Generic;
+
+    internal interface IConstIteratorAdapter<T, U> : IConstIterator<T, U>, IIterable, IEnumerable<KeyValuePair<T, U>>
+    {
+
+    }
+}
