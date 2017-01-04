8f03f41fd315dca7c46cde5f9e485f6c7ae6dfe3
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-04 19:01:38 +0100

    Add implementation for ArrayConstIterator<T>

--- Source/Lib/RangeIt/Iterators/Detail/Const/ArrayConstIterator'1.cs
+++ Source/Lib/RangeIt/Iterators/Detail/Const/ArrayConstIterator'1.cs
@@ -4,64 +4,91 @@
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
 
     internal struct ArrayConstIterator<T> : IConstIterator<T>
     {
+        private T[] _items;
+        private T _current;
+        private int _index;
+        private bool _isEnd;
+
         internal ArrayConstIterator(T[] items, bool isEnd = false)
         {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
 
+            _items = items;
+            _current = default(T);
+            _isEnd = isEnd;
+            _index = -1;
         }
 
-        public T Current
+        public T Current => _current;
+
+        public int Index => _index;
+
+        public bool IsEndIterator => _isEnd;
+
+        public bool IsValid => !IsEndIterator && Index >= 0 && Index < _items.Count();
+
+        public bool Previous()
         {
-            get
+            var count = _items.Count();
+
+            if (count == 0)
+                return false;
+
+            if (_index == 0)
             {
-                throw new NotImplementedException();
+                _isEnd = false;
+                _index = -1;
+                _current = default(T);
+                return false;
             }
-        }
 
-        public int Index
-        {
-            get
+            if (_isEnd)
+                _index = count;
+
+            if (_index >= 1)
             {
-                throw new NotImplementedException();
+                _isEnd = false;
+                --_index;
+                _current = _items[_index];
+                return true;
             }
-        }
 
-        public bool IsEndIterator
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public bool IsValid
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            return false;
         }
 
         public bool Next()
         {
-            throw new NotImplementedException();
+            var count = _items.Count();
+
+            if (_isEnd || count == 0)
+                return false;
+
+            if (_index == count - 1)
+            {
+                _index = count;
+                _isEnd = true;
+                _current = default(T);
+                return false;
+            }
+
+            if (_index < count - 1)
+            {
+                _index++;
+                _current = _items[_index];
+                return true;
+            }
+
+            _isEnd = true;
+            return false;
         }
 
-        public bool Previous()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerator<T> GetEnumerator() => _items.Cast<T>().GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
---
+++ Source/Tests/RangeIt.Tests/Iterators/Detail/Const/ArrayConstIterator'1_Tests.cs
@@ -0,0 +1,310 @@
+﻿namespace RangeIt.Tests.Iterators.Detail.Const
+{
+    using FluentAssertions;
+    using RangeIt.Iterators;
+    using System.Linq;
+    using Xunit;
+
+    [Collection("ArrayConstIterator<T>.Tests")]
+    public class ArrayConstIterator_1_Tests
+    {
+        [Fact]
+        public void Test_ArrayConstIterator_1_Begin_Ctor_WithEmptyArray()
+        {
+            var array = new int[0];
+            var it = array.ConstBegin();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_End_Ctor_WithEmptyArray()
+        {
+            var array = new int[0];
+            var it = array.ConstEnd();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_Begin_Iteration_WithEmptyArray()
+        {
+            var array = new int[0];
+            var it = array.ConstBegin();
+
+            it.Next().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+
+            it.Previous().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_End_Iteration_WithEmptyArray()
+        {
+            var array = new int[0];
+            var it = array.ConstEnd();
+
+            it.Next().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+
+            it.Previous().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_Begin_Ctor_WithNotEmptyArray()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var it = array.ConstBegin();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_End_Ctor_WithNotEmptyArray()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var it = array.ConstEnd();
+
+            it.Should().NotBeNull();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_Begin_Iteration_WithNotEmptyArray()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var it = array.ConstBegin();
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(0);
+            it.Current.Should().Be(1);
+
+            // 1. back iteration
+            it.Previous().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(0);
+            it.Current.Should().Be(1);
+
+            // 2. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(1);
+            it.Current.Should().Be(2);
+
+            // 3. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(2);
+            it.Current.Should().Be(3);
+
+            // 4. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(3);
+            it.Current.Should().Be(4);
+
+            // 5. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(4);
+            it.Current.Should().Be(5);
+
+            // 6. iteration
+            it.Next().Should().BeFalse();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(array.Count());
+            it.Current.Should().Be(0);
+
+            // 1. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(4);
+            it.Current.Should().Be(5);
+
+            // 2. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(3);
+            it.Current.Should().Be(4);
+
+            // 3. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(2);
+            it.Current.Should().Be(3);
+
+            // 4. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(1);
+            it.Current.Should().Be(2);
+
+            // 5. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(0);
+            it.Current.Should().Be(1);
+
+            // 6. back iteration
+            it.Previous().Should().BeFalse();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_End_Iteration_WithNotEmptyArray()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var it = array.ConstEnd();
+
+            // 1. iteration
+            it.Next().Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+
+            // 1. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(4);
+            it.Current.Should().Be(5);
+
+            // 2. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(3);
+            it.Current.Should().Be(4);
+
+            // 3. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(2);
+            it.Current.Should().Be(3);
+
+            // 4. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(1);
+            it.Current.Should().Be(2);
+
+            // 5. back iteration
+            it.Previous().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(0);
+            it.Current.Should().Be(1);
+
+            // 6. back iteration
+            it.Previous().Should().BeFalse();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.Current.Should().Be(0);
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(0);
+            it.Current.Should().Be(1);
+
+            // 2. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(1);
+            it.Current.Should().Be(2);
+
+            // 3. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(2);
+            it.Current.Should().Be(3);
+
+            // 4. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(3);
+            it.Current.Should().Be(4);
+
+            // 5. iteration
+            it.Next().Should().BeTrue();
+            it.Index.Should().Be(4);
+            it.Current.Should().Be(5);
+
+            // 6. iteration
+            it.Next().Should().BeFalse();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(array.Count());
+            it.Current.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_Begin_IsValid()
+        {
+            var array = new string[] { "a", "b", "c", "d", "e" };
+            var it = array.ConstBegin();
+
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(-1);
+            it.IsValid.Should().BeFalse();
+
+            // 1. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(0);
+            it.IsValid.Should().BeTrue();
+
+            // 2. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(1);
+            it.IsValid.Should().BeTrue();
+
+            // 3. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(2);
+            it.IsValid.Should().BeTrue();
+
+            // 4. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(3);
+            it.IsValid.Should().BeTrue();
+
+            // 5. iteration
+            it.Next().Should().BeTrue();
+            it.IsEndIterator.Should().BeFalse();
+            it.Index.Should().Be(4);
+            it.IsValid.Should().BeTrue();
+
+            // 6. iteration
+            it.Next().Should().BeFalse();
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(5);
+            it.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test_ArrayConstIterator_1_End_IsValid()
+        {
+            var array = new string[] { "a", "b", "c", "d", "e" };
+            var it = array.ConstEnd();
+
+            it.IsEndIterator.Should().BeTrue();
+            it.Index.Should().Be(-1);
+            it.IsValid.Should().BeFalse();
+        }
+    }
+}
