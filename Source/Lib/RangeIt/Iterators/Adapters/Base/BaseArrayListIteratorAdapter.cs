10ad69cf8ae52cf65b83fb4976e35e7e9d3e6c6c
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-05 22:06:44 +0100

    Remove unnecessary list counts

--- Source/Lib/RangeIt/Iterators/Adapters/Base/BaseArrayListIteratorAdapter.cs
+++ Source/Lib/RangeIt/Iterators/Adapters/Base/BaseArrayListIteratorAdapter.cs
@@ -10,6 +10,7 @@
         protected object _current;
         protected int _index;
         protected bool _isEnd;
+        protected int _count;
 
         internal BaseArrayListIteratorAdapter(ArrayList arrayList, bool isEnd = false)
         {
@@ -20,19 +21,18 @@
             _current = default(object);
             _isEnd = isEnd;
             _index = -1;
+            _count = _arrayList.Count;
         }
 
         public int Index => _index;
 
         public bool IsEndIterator => _isEnd;
 
-        public bool IsValid => !IsEndIterator && Index >= 0 && Index < _arrayList.Count;
+        public bool IsValid => !IsEndIterator && Index >= 0 && Index < _count;
 
         public bool Previous()
         {
-            var count = _arrayList.Count;
-
-            if (count == 0)
+            if (_count == 0)
                 return false;
 
             if (_index == 0)
@@ -44,7 +44,7 @@
             }
 
             if (_isEnd)
-                _index = count;
+                _index = _count;
 
             if (_index >= 1)
             {
@@ -59,20 +59,18 @@
 
         public bool Next()
         {
-            var count = _arrayList.Count;
-
-            if (_isEnd || count == 0)
+            if (_isEnd || _count == 0)
                 return false;
 
-            if (_index == count - 1)
+            if (_index == _count - 1)
             {
-                _index = count;
+                _index = _count;
                 _isEnd = true;
                 _current = default(object);
                 return false;
             }
 
-            if (_index < count - 1)
+            if (_index < _count - 1)
             {
                 _index++;
                 _current = _arrayList[_index];
