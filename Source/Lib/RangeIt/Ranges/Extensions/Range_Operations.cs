ae5d57ff346a19d2577ab8dfcce292972662a47c
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-01 21:47:55 +0100

    Add missing documentation comments

--- Source/Lib/RangeIt/Ranges/Extensions/Range_Operations.cs
+++ Source/Lib/RangeIt/Ranges/Extensions/Range_Operations.cs
@@ -4,9 +4,26 @@
 
     public static partial class Range
     {
+        /// <summary>
+        /// Applies a given filter <paramref name="function" /> to each element in the given
+        /// <paramref name="range" /> and returns a new range with elements, that are sufficient
+        /// to the condition of the given filter <paramref name="function" />.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the given <paramref name="range" />.</typeparam>
+        /// <param name="range">The range containing the elements, on which the given filter <paramref name="function" /> will be applied.</param>
+        /// <param name="function">The filter function, which will be applied to each element in the given <paramref name="range" />.</param>
+        /// <returns>A <see cref="Range{T}" />, containing the filtered elements.</returns>
         public static Range<T> Filter<T>(this Range<T> range, Func<T, bool> function)
             => range |= function;
 
+        /// <summary>
+        /// Applies a given <paramref name="function" /> to each element in the given
+        /// <paramref name="range" />.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the given <paramref name="range" />.</typeparam>
+        /// <param name="range">The range containing the elements, on which the given <paramref name="function" /> will be applied.</param>
+        /// <param name="function">The function, which will be applied to each element in the given <paramref name="range" />.</param>
+        /// <returns>A <see cref="Range{T}" />, containing the transformed elements.</returns>
         public static Range<T> Transform<T>(this Range<T> range, Func<T, T> function)
             => range |= function;
     }
--- Source/Lib/RangeIt/Ranges/Range'1.cs
+++ Source/Lib/RangeIt/Ranges/Range'1.cs
@@ -5,6 +5,8 @@
     using System.Collections;
     using System.Collections.Generic;
 
+    /// <summary>A range collection containing elements of type <typeparamref name="T" />.</summary>
+    /// <typeparam name="T">The type of the elements in this range.</typeparam>
     public struct Range<T> : IEnumerable<T>, IEquatable<Range<T>>
     {
         private IRangeStrategy<T> _strategy;
@@ -17,20 +19,55 @@
             _strategy = strategy;
         }
 
+        /// <summary>Returns a <see cref="IEnumerator{T}" /> for this range.</summary>
+        /// <returns>A <see cref="IEnumerator{T}" />.</returns>
         public IEnumerator<T> GetEnumerator() => _strategy.GetEnumerator();
 
+        /// <summary>Returns a <see cref="IEnumerator" /> for this range.</summary>
+        /// <returns>A <see cref="IEnumerator" />.</returns>
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        /// <summary>Indicates whether this range is equal to the given range <paramref name="other" />.</summary>
+        /// <param name="other">The range, which will be compared to this range.</param>
+        /// <returns>
+        /// True, if the given range <paramref name="other" />
+        /// is equal to this range, otherwise false.
+        /// </returns>
         public bool Equals(Range<T> other) => other._strategy.Equals(_strategy);
 
+        /// <summary>Returns a string representation of this range as comma-separated list.</summary>
+        /// <returns>A string representation of this range as comma-separated list.</returns>
+        /// <seealso cref="ToString(string)" />
         public override string ToString() => ToString(",");
 
+        /// <summary>
+        /// Returns a string representation of this range as a list separated
+        /// by a given <paramref name="separator" />.
+        /// </summary>
+        /// <param name="separator">A separator to be placed between each element in the range string representation.</param>
+        /// <returns>A string representation of this range as comma-separated list.</returns>
+        /// <seealso cref="ToString()" />
         public string ToString(string separator) => string.Join(separator, _strategy);
 
-        public static Range<T> operator |(Range<T> lhs, Func<T, T> function)
-            => new Range<T>(new RangeTransform<T>(lhs, function));
+        /// <summary>
+        /// Applies a given <paramref name="function" /> to each element in the given
+        /// <paramref name="range" />.
+        /// </summary>
+        /// <param name="range">The range containing the elements, on which the given <paramref name="function" /> will be applied.</param>
+        /// <param name="function">The function, which will be applied to each element in the given <paramref name="range" />.</param>
+        /// <returns>A <see cref="Range{T}" />, containing the transformed elements.</returns>
+        public static Range<T> operator |(Range<T> range, Func<T, T> function)
+            => new Range<T>(new RangeTransform<T>(range, function));
 
-        public static Range<T> operator |(Range<T> lhs, Func<T, bool> function)
-            => new Range<T>(new RangeFilter<T>(lhs, function));
+        /// <summary>
+        /// Applies a given filter <paramref name="function" /> to each element in the given
+        /// <paramref name="range" /> and returns a new range with elements, that are sufficient
+        /// to the condition of the given filter <paramref name="function" />.
+        /// </summary>
+        /// <param name="range">The range containing the elements, on which the given filter <paramref name="function" /> will be applied.</param>
+        /// <param name="function">The filter function, which will be applied to each element in the given <paramref name="range" />.</param>
+        /// <returns>A <see cref="Range{T}" />, containing the filtered elements.</returns>
+        public static Range<T> operator |(Range<T> range, Func<T, bool> function)
+            => new Range<T>(new RangeFilter<T>(range, function));
     }
 }
